cc2b26d165b7a81d32e5b4e10e29d1196a89d50c
Durgesh <durgesh9491@gmail.com>
2017-09-25 19:00:36 +0530

    Displaying microposts for a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -39,14 +40,6 @@ class UsersController < ApplicationController
 
   def index
      @users = User.paginate(page: params[:page])
-  end
-
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
   end
 
   # Confirms the correct user.
